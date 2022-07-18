47835fff3069b9a3351e21292a6e8e644f74cfb3
Anderson Moura <86577024+AndersonMoura333@users.noreply.github.com>
2022-07-18 16:47:50 -0300

    função trailer filmes add

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,17 +1,46 @@
+import movieTrailer from 'movie-trailer';
 import React, { useState, useEffect} from 'react'
+import ReactPlayer from 'react-player';
+import YouTube from 'react-youtube';
 import { getMoveis } from '../api';
 import styles from '../styles/Row.module.css'
 const imageHost = "https://image.tmdb.org/t/p/original/";
 export default function Row({title, path, isLarge}) {
 const [movies, setMovies] = useState([]);
+const [trailerUrl, setTrailerUrl] = useState("");
 
-const fetchMovies =async (path) => {
+
+
+
+
+const opts = {
+  maxResults: 10,
+  key: 'thor 4'
+};
+
+const fetchMovies = async (path) => {
     try {
         const data = await getMoveis(path);
         setMovies (data?.results);
     } catch (error) {
         console.log("fetchMovies error", error);
     }
+}
+
+const handleOnClick = (movie) =>{
+
+  if (trailerUrl) {
+    setTrailerUrl("");
+  }else{
+
+    movieTrailer(movie.title || movie.name || movie.original_name || "")
+    .then((url) =>{
+      setTrailerUrl(url)
+     
+    }).catch(error => console.log("Error fetching movie trailer: ", error))
+  
+  }
+
 }
 
 useEffect(() => {
@@ -24,7 +53,8 @@ useEffect(() => {
         <div className={styles.rowCards}>
             {movies?.map((movie) =>{
                 return (
-                <img   
+                <img
+                onClick={() => handleOnClick(movie)}
                 className={`${styles.movieCard}  ${(isLarge && styles.movieCardLarge)}`}
                  key = {movie.id}
                  src={`${imageHost}${
@@ -33,6 +63,8 @@ useEffect(() => {
                  alt=""  />)
             })}
         </div>
+        {/* {trailerUrl && <YouTube videoId="2g811Eo7K8U" opts={opts} />} */}
+        {trailerUrl && <ReactPlayer url={trailerUrl}  playing = {true}/>}
     </div>
   )
 }
